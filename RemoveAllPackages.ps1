3e13bdee8d89dd2f367cc2237414587b04c802a6
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-07-29 10:09:49 +0300

    Add script RemoveAllPackages.ps1

---
+++ RemoveAllPackages.ps1
@@ -0,0 +1,28 @@
+param(
+    $packageNamePattern = "*"
+)
+
+if (!$match) {
+    $packageNamePattern = $packageNamePattern.Replace(".", "`\.").Replace("*", ".*")
+    "Package name pattern: '$packageNamePattern'"
+}
+
+Function RemovePackages {
+    param (
+        $fileName
+    )
+    "Remove packages by pattern '$packageNamePattern' in file '$fileName'"
+    $directoryPath = Split-Path -Path $fileName -Parent
+    Push-Location $directoryPath
+    Select-String -Path $fileName `
+        -Pattern "<PackageReference Include=\`"($packageNamePattern)\`" Version" `
+    | % { $_.Matches } `
+    | % { $_.Groups[1].Value } `
+    | % { . dotnet.exe remove package $_ }
+    Pop-Location
+}
+
+dotnet nuget locals http-cache --clear
+Get-ChildItem -Include "*.csproj", "*.fsproj" -Recurse `
+| Select-String "<PackageReference Include=`"$packageNamePattern`"" -List `
+| % { RemovePackages $_.Path }
