51b4a499e2adf1f6c6afeb06bd3d0935f45388f7
Gennadii Saltyshchak <GennadyGS@gmail.com>
2023-01-13 08:57:11 +0200

    Add script UpdateAllPackagesFrom.ps1

---
+++ UpdateAllPackagesFrom.ps1
@@ -0,0 +1,41 @@
+param(
+    $solutionOrDirectory,
+    $version,
+    $framework,
+    $source,
+    [switch]$prerelease
+)
+
+Function TryGetAssemblyName($projectRelativePath) {
+    $fullPath = Join-Path $solutionDirectory $projectRelativePath
+    if (!(Test-Path $fullPath -PathType Leaf)) {
+        return $null
+    }
+    $content = [string](Get-Content $fullPath)
+    if ($content -match "<IsPackable>false</IsPackable>") {
+        return $null
+    }
+    $baseProjectName = [IO.Path]::GetFileNameWithoutExtension($projectRelativePath)
+    $content -match "<AssemblyName>(?<assemblyName>.*)</AssemblyName>" `
+        ? $Matches["assemblyName"].Replace("`$(MSBuildProjectName)", $baseProjectName) `
+        : $baseProjectName
+}
+
+$solutionDirectory = (Test-Path $solutionOrDirectory -PathType Leaf) `
+    ? [IO.Path]::GetDirectoryName($solutionOrDirectory) `
+    : $solutionOrDirectory
+
+$packageNames = . dotnet sln $solutionOrDirectory list `
+    | ForEach-Object { TryGetAssemblyName $_ }
+    | Where-Object { $_ }
+    | Sort-Object
+
+$packageNamePattern = ($packageNames | ForEach-Object { [Regex]::Escape($_) }) -join "|"
+
+. $PSScriptRoot/UpdateAllPackages.ps1 `
+    $packageNamePattern `
+    -match `
+    -version=$version `
+    -framework=$framework `
+    -source=$source `
+    -prerelease:$prerelease
