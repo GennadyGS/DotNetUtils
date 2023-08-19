00a70a451c2ea7de3d40e8cc4d3fe897169ae441
Gennadii Saltyshchak <GennadyGS@gmail.com>
2023-08-19 18:48:52 +0300

    Add script AddPackage

--- AddPackage.ps1
+++ AddPackage.ps1
@@ -1,51 +1,37 @@
 param(
-    $packageNamePattern = "*",
+    [Parameter(Mandatory=$true)] $packageName,
     $version,
     $targetPath = ".",
     $packageSource,
     [Alias("f")] $framework,
     [switch] $build,
     [switch] $test,
-    [switch] [Alias("pre")] $prerelease,
-    [switch] $match
+    [switch] [Alias("pre")] $prerelease
 )
 
 . $PSScriptRoot\Common.ps1
-
-if (!$match) {
-    $packageNamePattern = $packageNamePattern.Replace(".", "`\.").Replace("*", "[\w\.]*")
-}
 
 if ($version) { $versionParam = "-v:$version" }
 if ($packageSource) { $sourceParam = "-s:$packageSource" }
 if ($framework) { $frameworkParam = "-f:$framework" }
 if ($prerelease) { $prereleaseParam = "--prerelease" }
-$updated = $false
 
-Function UpdatePackages {
+Function AddPackage {
     param (
         $fileName
     )
 
-    Write-Host "Updating packages by pattern '$packageNamePattern' in project '$fileName' ..." `
+    Write-Host "Adding package '$packageName' to project '$fileName' ..." `
         -ForegroundColor $commandColor
-    Select-String -Path $fileName -Pattern "<PackageReference Include=\`"($packageNamePattern)\`"" `
-    | ForEach-Object { $_.Matches } `
-    | ForEach-Object { $_.Groups[1].Value } `
-    | ForEach-Object {
-        $script:updated = $true
-        RunAndLogCommand dotnet.exe add $fileName package $_ `
+    RunAndLogCommand `
+        dotnet.exe add $fileName package $packageName `
             $versionParam $frameworkParam $sourceParam $prereleaseParam
-    }
 }
 
 & dotnet nuget locals http-cache --clear
 Get-ChildItem -Path $targetPath -Include "*.csproj", "*.fsproj" -Recurse `
 | Where-Object { (Get-Content $_) -match "Sdk=""Microsoft.NET.Sdk" }
-| Select-String "<PackageReference Include=`"$packageNamePattern`"" -List `
-| ForEach-Object { UpdatePackages $_.Path }
-
-if (!$updated) { return }
+| ForEach-Object { AddPackage $_ }
 
 if ($test) { RunAndLogCommand dotnet test $targetPath }
 elseif ($build) { RunAndLogCommand dotnet build $targetPath }
